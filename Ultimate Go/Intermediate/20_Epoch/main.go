4356aeb3efc065adde15aaaf91dcd2952a7d1938
Shawshank44 <shashankkoushik44@gmail.com>
2025-06-01 19:51:37 +0530

    Added Epoch and Time parsing

---
+++ Ultimate Go/Intermediate/20_Epoch/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	//00:00:00 UTC on January 1, 1970
+
+	now := time.Now()
+	unixTime := now.Unix()
+
+	fmt.Println("Current unix time", unixTime)
+
+	t := time.Unix(unixTime, 0)
+	fmt.Println(t)
+	fmt.Println("Time : ", t.Format("2006-01-02"))
+
+}
