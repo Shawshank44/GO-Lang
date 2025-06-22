b3f73d70bcb17004701a888735d7186fde07cd47
Shawshank44 <shashankkoushik44@gmail.com>
2025-06-22 12:39:59 +0530

    Added Temporary files and ember directive

---
+++ Ultimate Go/Intermediate/33_Temporary files and directories/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	// Making a temporary file
+	// Tempfile, err := os.CreateTemp("tempdir", "Tempfile")
+	// CheckError(err)
+
+	// fmt.Println(Tempfile.Name())
+	// defer os.RemoveAll("tempdir")
+	// defer Tempfile.Close()
+
+	// Making a temporary directory :
+	tempdir, err := os.MkdirTemp("", "GO course temp dir")
+	CheckError(err)
+
+	defer os.RemoveAll(tempdir)
+	fmt.Println("Temporary directory created: ", tempdir)
+
+}
---
+++ Ultimate Go/Intermediate/34_Embed Directive/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"embed" // imported but not using directly (blank import)
+	"fmt"
+	"io/fs"
+)
+
+//go:embed example.txt
+var content string
+
+//go:embed baser
+var baserfolder embed.FS
+
+func checkerror(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	fmt.Println("Embeded content : ", content)
+	content, err := baserfolder.ReadFile("baser/hello.txt")
+	checkerror(err)
+
+	fmt.Println("Embeded file content ", string(content))
+
+	// listing the folder content :
+	err = fs.WalkDir(baserfolder, "baser", func(path string, d fs.DirEntry, err error) error {
+		checkerror(err)
+		fmt.Println(path)
+		return nil
+	})
+	checkerror(err)
+
+}
