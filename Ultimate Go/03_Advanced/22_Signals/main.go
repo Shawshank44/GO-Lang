87ec2fafa619789a9843649bfe16c5d04bed2276
Shawshank44 <shashankkoushik44@gmail.com>
2025-08-10 11:56:55 +0530

    Added Signals

---
+++ Ultimate Go/03_Advanced/22_Signals/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	pid := os.Getpid()
+
+	fmt.Println(pid)
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	// Notify channel on interrupt or terminate signals
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP) // for SIGTERM command is (taskkill /PID {Generated pid} /F)
+	// In windows we cannot directly use the SIGTERM and SIGINT command.
+	go func() {
+		sig := <-sigs
+		fmt.Println("Received signal", sig)
+		done <- true
+	}()
+	go func() {
+		for {
+			select {
+			case <-done:
+				fmt.Println("Stopping work due to signal")
+				return
+			default:
+				fmt.Println("Working...")
+				time.Sleep(time.Second)
+			}
+		}
+		// sig := <-sigs
+		// switch sig {
+		// case syscall.SIGINT:
+		// 	fmt.Println("Received SIGINT (INTERRUPT)")
+		// case syscall.SIGTERM:
+		// 	fmt.Println("Received SIGTERM (TERMINATE)")
+		// case syscall.SIGHUP:
+		// 	fmt.Println("Received SIGHUP (HUNGUP)")
+		// }
+		// fmt.Println("Graceful Exit")
+		// os.Exit(0)
+	}()
+
+	// Simulate some work
+	// fmt.Println("Working...")
+	for {
+		time.Sleep(time.Second)
+	}
+}
