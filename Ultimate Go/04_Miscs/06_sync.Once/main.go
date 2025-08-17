f55813877f46ae998d352b058df6fa34c1b6789d
Shawshank44 <shashankkoushik44@gmail.com>
2025-08-17 12:03:21 +0530

    Added sync.Once, sync.Pool and for select statement

---
+++ Ultimate Go/04_Miscs/06_sync.Once/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	once sync.Once
+)
+
+func Initialize() {
+	fmt.Println("This will not be repeated no matter how many time we call this function using once.do")
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := range 5 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Printf("Go Routine %d \n", i)
+			once.Do(Initialize) // this will only run once no matter how many Go routines are there to execute.
+		}()
+	}
+
+	wg.Wait()
+}
---
+++ Ultimate Go/04_Miscs/07_sync.Pool/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Person struct {
+	name string
+	age  int
+}
+
+/*
+	sync.Pool, which is a concurrency-safe pool of temporary objects that can be reused to reduce allocation overhead and improve performance.
+*/
+
+func main() {
+	var pool = sync.Pool{
+		New: func() any {
+			fmt.Println("Creating a new person.")
+			return &Person{}
+		},
+	}
+
+	// Get an object from the pool
+	person := pool.Get().(*Person)
+	person.name = "John"
+	person.age = 18
+	fmt.Println("Get Person : ", person)
+	fmt.Printf("Name : %s, Age : %d \n", person.name, person.age)
+	pool.Put(person)
+	fmt.Println("Returned person to pool")
+
+	person1 := pool.Get().(*Person)
+	fmt.Println("Get Person again", person1)
+
+	person2 := pool.Get().(*Person)
+	fmt.Println("Get another person.", person2)
+
+	// Returning object to the pool again
+	pool.Put(person1)
+	pool.Put(person2)
+	fmt.Println("Returned Persons to pool")
+
+	person3 := pool.Get().(*Person)
+	fmt.Println("Get person : ", person3)
+	person3.name = "Lupa"
+	pool.Put(person3)
+
+	person4 := pool.Get().(*Person)
+	fmt.Println("Get Person : ", person4)
+}
