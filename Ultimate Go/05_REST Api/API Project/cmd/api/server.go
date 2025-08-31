e1808c12f365c0cf4bb5d0d6381bd995f6fe64f3
Shawshank44 <shashankkoushik44@gmail.com>
2025-08-31 19:13:10 +0530

    Added Request Processing and updated the Routes.

--- Ultimate Go/05_REST Api/API Project/cmd/api/server.go
+++ Ultimate Go/05_REST Api/API Project/cmd/api/server.go
@@ -30,12 +30,51 @@ func main() {
 			w.Write([]byte("Welcome to Teacher DELETE"))
 			return
 		}
+
 		w.Write([]byte("Welcome to Teacher Page"))
 	})
 	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Method)
+		switch r.Method {
+		case http.MethodGet:
+			w.Write([]byte("Welcome to student GET"))
+			return
+		case http.MethodPost:
+			w.Write([]byte("Welcome to student POST"))
+			return
+		case http.MethodPut:
+			w.Write([]byte("Welcome to student PUT"))
+			return
+		case http.MethodPatch:
+			w.Write([]byte("Welcome to student PATCH"))
+			return
+		case http.MethodDelete:
+			w.Write([]byte("Welcome to student DELETE"))
+			return
+		}
+
 		w.Write([]byte("Hello Welcome Student"))
 	})
 	http.HandleFunc("/execs", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Method)
+		switch r.Method {
+		case http.MethodGet:
+			w.Write([]byte("Welcome to Executive GET"))
+			return
+		case http.MethodPost:
+			w.Write([]byte("Welcome to Executive POST"))
+			return
+		case http.MethodPut:
+			w.Write([]byte("Welcome to Executive PUT"))
+			return
+		case http.MethodPatch:
+			w.Write([]byte("Welcome to Executive PATCH"))
+			return
+		case http.MethodDelete:
+			w.Write([]byte("Welcome to Executive DELETE"))
+			return
+		}
+
 		w.Write([]byte("Hello Welcome Executives"))
 	})
 
