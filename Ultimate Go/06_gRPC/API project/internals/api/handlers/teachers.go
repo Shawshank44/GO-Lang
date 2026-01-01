1145ae83e695e9226fef57efa33dc3a0693f4e69
Shawshank44 <shashankkoushik44@gmail.com>
2026-01-01 14:22:32 +0530

    Added Delete Teachers

--- Ultimate Go/06_gRPC/API project/internals/api/handlers/teachers.go
+++ Ultimate Go/06_gRPC/API project/internals/api/handlers/teachers.go
@@ -2,10 +2,14 @@ package handlers
 
 import (
 	"context"
+	"errors"
 	"gRPC_school_api/internals/models"
 	"gRPC_school_api/internals/repositories/mongodb"
+	"gRPC_school_api/pkg/utils"
 	pb "gRPC_school_api/proto/gen"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -47,4 +51,52 @@ func (s *Server) UpdateTeachers(ctx context.Context, req *pb.Teachers) (*pb.Teac
 		return nil, status.Error(codes.Internal, err.Error())
 	}
 	return &pb.Teachers{Teachers: updatedTeachers}, nil
+}
+
+func (s *Server) DeleteTeachers(ctx context.Context, req *pb.TeacherIDs) (*pb.DeleteTeachersConfirmation, error) {
+	ids := req.GetIds()
+	var teacherIdsToDelete []string
+
+	for _, v := range ids {
+		if v.Id == "" {
+			return nil, errors.New("id Field cannot be blank")
+		}
+		teacherIdsToDelete = append(teacherIdsToDelete, v.Id)
+	}
+
+	client, err := mongodb.CreateMongoClient()
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "Internal error")
+	}
+	defer client.Disconnect(ctx)
+
+	objectIds := make([]primitive.ObjectID, len(teacherIdsToDelete))
+	for i, id := range teacherIdsToDelete {
+		objectId, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return nil, utils.ErrorHandler(err, "Invalid ID")
+		}
+		objectIds[i] = objectId
+	}
+
+	filter := bson.M{"_id": bson.M{"$in": objectIds}}
+
+	res, err := client.Database("School").Collection("teachers").DeleteMany(ctx, filter)
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "Error in deleting the _Id")
+	}
+
+	if res.DeletedCount == 0 {
+		return nil, utils.ErrorHandler(err, "No teachers were deleted")
+	}
+
+	deletedIds := make([]string, res.DeletedCount)
+	for i, id := range objectIds {
+		deletedIds[i] = id.Hex()
+	}
+
+	return &pb.DeleteTeachersConfirmation{
+		Status:     "Teachers Successfully Deleted",
+		DeletedIds: deletedIds,
+	}, nil
 }
