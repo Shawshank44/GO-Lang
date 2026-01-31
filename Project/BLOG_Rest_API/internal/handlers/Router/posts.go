85d221fadea388acb08fa47478fed2c14bfc8261
Shawshank44 <shashankkoushik44@gmail.com>
2026-01-31 17:49:33 +0530

    Refining routes.(users, posts)

--- Project/BLOG_Rest_API/internal/handlers/Router/posts.go
+++ Project/BLOG_Rest_API/internal/handlers/Router/posts.go
@@ -1,12 +1,37 @@
 package router
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func PostsRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /posts", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /getposts", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to Posts page"))
+	})
+	mux.HandleFunc("GET /getpost/{id}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to get post by id", r.PathValue("id"))
+	})
+
+	mux.HandleFunc("POST /createpost", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to create post page"))
+	})
+
+	mux.HandleFunc("PATCH /updateposts", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to update posts page"))
+	})
+	mux.HandleFunc("PATCH /updatepost/{id}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to Update post by ID page", r.PathValue("id"))
+	})
+
+	mux.HandleFunc("DELETE /deleteposts", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to delete post page"))
+	})
+
+	mux.HandleFunc("DELETE /deletepost/{id}", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to delete post by ID page"))
 	})
 
 	return mux
--- Project/BLOG_Rest_API/internal/handlers/Router/users.go
+++ Project/BLOG_Rest_API/internal/handlers/Router/users.go
@@ -1,12 +1,48 @@
 package router
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func UsersRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /users", func(w http.ResponseWriter, r *http.Request) {
+	// Read
+	mux.HandleFunc("GET /getusers", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to Users page"))
+	})
+	mux.HandleFunc("GET /getusers/{id}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to users by id page.", r.PathValue("id"))
+	})
+	// Create
+	mux.HandleFunc("POST /users/register", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to user register page"))
+	})
+	mux.HandleFunc("POST /users/login", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to login page"))
+	})
+	mux.HandleFunc("POST /users/logout", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to logout page"))
+	})
+
+	//Update
+	mux.HandleFunc("PATCH /updateusers/{id}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to update users by id", r.PathValue("id"))
+	})
+
+	// Delete
+	mux.HandleFunc("DELETE /users/deactivate", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to delete page"))
+	})
+
+	// MISCs:
+	mux.HandleFunc("POST /users/forgotpassword", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to forgot password page"))
+	})
+
+	mux.HandleFunc("POST /users/resetpassword", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to resetpassword page"))
 	})
 
 	return mux
