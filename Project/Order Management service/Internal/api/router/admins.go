23c59116e166243847d7347377a61387f482c20a
Shawshank44 <shashankkoushik44@gmail.com>
2026-07-11 12:12:08 +0530

    Added Admin Router

--- Project/Order Management service/Internal/api/router/admins.go
+++ Project/Order Management service/Internal/api/router/admins.go
@@ -8,8 +8,44 @@ import (
 func AdminRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /getadmins", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Welcome to admins page")
+	// Read :
+	mux.HandleFunc("GET /api/admin/super/getadmins", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to getadmin page"))
+	})
+	mux.HandleFunc("GET /api/admin/super/getadmin/{id}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to get admins by id page : ", r.PathValue("id"))
+	})
+
+	// Create :
+	mux.HandleFunc("POST /api/admin/super/register", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to admin register page"))
+	})
+	mux.HandleFunc("POST /api/admin/super/login", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to admin login page"))
+	})
+	mux.HandleFunc("POST /api/admin/super/logout", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to admin logout page"))
+	})
+
+	// Update :
+	mux.HandleFunc("PATCH /api/admin/super/updateadmin/{id}", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to admin update page"))
+	})
+
+	// Delete :
+	mux.HandleFunc("DELETE /api/admin/super/deactivate", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to admin deactivate page"))
+	})
+	mux.HandleFunc("DELETE /api/admin/super/deactivateusers", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to admin deactivateusers page"))
+	})
+
+	// MISCS :
+	mux.HandleFunc("POST /api/admin/super/forgotpassword", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to admin forgotpassword page"))
+	})
+	mux.HandleFunc("POST /api/admin/super/resetpassword", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to admin resetpassword page"))
 	})
 
 	return mux
