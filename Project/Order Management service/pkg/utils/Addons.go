51a79072f6fb5b4494767873251ada33c92a9a3e
Shawshank44 <shashankkoushik44@gmail.com>
2026-07-19 13:07:29 +0530

    Added Addons

---
+++ Project/Order Management service/pkg/utils/Addons.go
@@ -0,0 +1,73 @@
+package utils
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func IsValidSortOrder(order string) bool {
+	return order == "asc" || order == "desc"
+}
+
+func IsValidSortField(field string) bool {
+	validFields := map[string]bool{
+		"username": true,
+		"email":    true,
+	}
+	return validFields[field]
+}
+
+func AddSorting(r *http.Request, query string) string {
+	sortParams := r.URL.Query()["sortby"]
+
+	if len(sortParams) > 0 {
+		query += " ORDER BY"
+		for i, param := range sortParams {
+			parts := strings.Split(param, ":")
+			if len(parts) != 2 {
+				continue
+			}
+			fields, order := parts[0], parts[1]
+			if !IsValidSortField(fields) || !IsValidSortOrder(order) {
+				continue
+			}
+			if i > 0 {
+				query += ","
+			}
+			query += " " + fields + " " + order
+		}
+	}
+	return query
+}
+
+func AddFilters(r *http.Request, query string, args []interface{}) (string, []interface{}) {
+	params := map[string]string{
+		"username": "username",
+		"email":    "email",
+	}
+
+	for params, dbfield := range params {
+		value := r.URL.Query().Get(params)
+
+		if value != "" {
+			query += " AND " + dbfield + " = ?"
+			args = append(args, value)
+		}
+	}
+	return query, args
+}
+
+func GetPaginationParams(r *http.Request) (int, int) {
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+	if err != nil {
+		page = 1
+	}
+
+	limit, err := strconv.Atoi(r.URL.Query().Get("limit"))
+	if err != nil {
+		limit = 10
+	}
+
+	return page, limit
+}
