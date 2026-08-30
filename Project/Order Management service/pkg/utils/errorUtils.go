bc46c4db8d6f91bf7e36f424262b7a188b96268b
Shawshank44 <shashankkoushik44@gmail.com>
2026-08-30 17:18:30 +0530

    Updated error handler function for better logging

--- Project/Order Management service/pkg/utils/errorUtils.go
+++ Project/Order Management service/pkg/utils/errorUtils.go
@@ -6,8 +6,13 @@ import (
 	"os"
 )
 
+var errLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
 func ErrorHandler(err error, message string) error {
-	errLogger := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-	errLogger.Println(message, err)
-	return fmt.Errorf("%v", message)
+	if err == nil {
+		return nil
+	}
+	errLogger.Printf("%s: %v", message, err)
+
+	return fmt.Errorf("%s: %w", message, err)
 }
